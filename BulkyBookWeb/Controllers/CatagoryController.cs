cc91100332761403444910279bfedbc62990f8ad
AQA98 <abdulqadir199853@gmail.com>
2023-08-09 17:26:04 -0400

    Bulky book web - function to watch existing catagories

---
+++ BulkyBookWeb/Controllers/CatagoryController.cs
@@ -0,0 +1,27 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CatagoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CatagoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Catagory> objCatagory = _db.Catagories;
+            return View(objCatagory);   
+        }
+
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+    }
+}
